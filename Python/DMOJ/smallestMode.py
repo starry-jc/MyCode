6f2423b695f7addb262326bed5f1871a0d0c39d3
☆Alisha•陈★ <alisha.j.chen@gmail.com>
2026-07-28 17:41:42 -0400

    solving problem "smallestMode" - 7/28, 5:41pm

--- Python/DMOJ/smallestMode.py
+++ Python/DMOJ/smallestMode.py
@@ -1,3 +1,26 @@
 # Problem: nccc5j2
 
-i love popcorn <3
+n = int(input())
+a_i = input()
+
+a_i_list = a_i.split()
+seen_vals = set()
+removed_vals = []
+
+for i in a_i_list:
+    if i in seen_vals:
+        removed_vals.append(i)
+    else:
+        seen_vals.add(i)
+
+map(int, removed_vals)
+
+print(removed_vals)
+
+'''for j in range(n):
+    int(a_i_list[j])
+    for k in range(n):
+        if a_i_list[j] == a_i_list[k]:
+            repeated_vals.append(a_i_list[j])
+
+print(repeated_vals)'''
