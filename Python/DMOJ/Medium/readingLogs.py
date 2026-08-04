268b72e84f3e962e46fffcd0c43ac1d9dbaa4fee
☆Alisha•陈★ <alisha.j.chen@gmail.com>
2026-08-04 18:32:07 -0400

    started solving problem "reading logs" - 6:31pm, 8/4/26

---
+++ Python/DMOJ/Medium/readingLogs.py
@@ -0,0 +1,19 @@
+# Problem: cspc1p4
+
+# Assigning variables
+
+# d - days left in break
+d = int(input())
+# p - number of pages in book
+p = int(input())
+# class_code - class code as a string
+class_code = input()
+# n - number of students in class
+n = int(input())
+# i_list - n inputs including student name,
+#  daily number of pages read, and class code
+i_list = []
+
+for i in range(n):
+    i_list.append(input())
+
