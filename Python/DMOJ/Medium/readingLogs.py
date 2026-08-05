0d30526382e54fd6e651948b41be7de7e1d77ca5
☆Alisha•陈★ <alisha.j.chen@gmail.com>
2026-08-04 23:41:19 -0400

    solving problem "reading logs" - 11:41pm, 8/4/26

--- Python/DMOJ/Medium/readingLogs.py
+++ Python/DMOJ/Medium/readingLogs.py
@@ -14,6 +14,43 @@ n = int(input())
 #  daily number of pages read, and class code
 i_list = []
 
-for i in range(n):
-    i_list.append(input())
 
+i_name = []
+i_wpd = []
+i_class_code = []
+
+
+for i in range(n):
+    i_list.append(input().split())
+    
+    i_name.append(i_list[-1][0])
+    i_wpd.append(i_list[-1][1])
+    i_class_code.append(i_list[-1][2])
+
+efficient_readers = []
+
+try:
+    for j in range(len(i_name)):
+        if i_class_code[j] != class_code:
+            i_name.remove(i_name[j])
+            i_wpd.remove(i_wpd[j])
+            i_class_code.remove(i_class_code[j])
+            
+        try:
+            if round(p / d) == int(i_wpd[j]):
+                efficient_readers.append(i_name[j])
+        except ZeroDivisionError:
+            continue
+except IndexError:
+    pass
+        
+
+
+if len(efficient_readers) == 1:
+    print(f"The most efficient reader is {efficient_readers[0]}.")
+    print("This reader is perfectly efficient.")
+elif len(efficient_readers) == 0:
+    print("None of the readers are perfectly efficient.")
+else:
+    print(f"The most efficient readers are {",".join(efficient_readers)}.")
+    print("These readers are perfectly efficient.")
