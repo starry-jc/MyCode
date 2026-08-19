3c0251771cbade7b71ced2a60354330047536d19
Alisha 陈.♡ <alisha.j.chen@gmail.com>
2026-08-19 17:37:37 -0400

    revised problem "alphabet score" - 5:37pm, 8/19/26

--- Python/DMOJ/Easy/alphabetScore.py
+++ Python/DMOJ/Easy/alphabetScore.py
@@ -14,44 +14,42 @@ score = 0
 # alphabet_dict - creates a dictionary
 #  where each key is paired with an alphabet value
 alphabet_dict = {
-    1: 'a',
-    2: 'b',
-    3: 'c',
-    4: 'd',
-    5: 'e',
-    6: 'f',
-    7: 'g',
-    8: 'h',
-    9: 'i',
-    10: 'j',
-    11: 'k',
-    12: 'l',
-    13: 'm',
-    14: 'n',
-    15: 'o',
-    16: 'p',
-    17: 'q',
-    18: 'r',
-    19: 's',
-    20: 't',
-    21: 'u',
-    22: 'v',
-    23: 'w',
-    24: 'x',
-    25: 'y',
-    26: 'z'
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'h': 8,
+    'i': 9,
+    'j': 10,
+    'k': 11,
+    'l': 12,
+    'm': 13,
+    'n': 14,
+    'o': 15,
+    'p': 16,
+    'q': 17,
+    'r': 18,
+    's': 19,
+    't': 20,
+    'u': 21,
+    'v': 22,
+    'w': 23,
+    'x': 24,
+    'y': 25,
+    'z': 26
 }
 
 # iterates through the length of s:
-#  iterates through the keys and values of
-#  alphabet_dict:
-#    Checks to see if index i in the list of
-#    s is equal to the value inside alphabet_dict.
-#      If true, score will add the key each time.
+#  Checks to see if the element at index i of s
+#  is inside of alphabet_dict
+#    If so, the score will add the value found at
+#    the key of the element at index i of s to itself
 for i in range(len(s)):
-    for key, value in alphabet_dict.items():
-        if s[i] == value:
-            score += key
+    if s[i] in alphabet_dict:
+        score += alphabet_dict[s[i]]
 
 # Outputs the score
 print(score)
