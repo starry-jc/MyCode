f3d4b1dea7fdd632e27a8c419b706d21ccf5ed82
☆Alisha•陈★ <alisha.j.chen@gmail.com>
2026-08-23 22:46:32 -0400

    solving problem "olympic scoring" - 8/23/26

--- Python/DMOJ/Easy/olympicScoring.py
+++ Python/DMOJ/Easy/olympicScoring.py
@@ -6,25 +6,27 @@ n, b, s, g = map(int, input().split())
 
 canada_score = b * 1 + s * 3 + g * 5
 num_medals = []
-min_medals = 0
 
-remaining_score = n - canada_score
+remaining_score = n - canada_score + 1
+
+min_medals = math.ceil(remaining_score / 5)
+print(min_medals)
 
 '''print(remaining_score)'''
 
-if remaining_score < 0:
+'''if remaining_score < 1:
     print(0)
-elif remaining_score == 0:
+elif remaining_score == 1:
     print(1)
 else:
     num_medals.append(math.ceil(remaining_score / 1))
     num_medals.append(math.ceil(remaining_score / 3))
     num_medals.append(math.ceil(remaining_score / 5))
     
-    '''print(num_medals)'''
+    print(num_medals)
 
-    '''for i in range(len(num_medals)):
+    for i in range(len(num_medals)):
         if num_medals[i] < min_medals or min_medals == 0:
-            min_medals = num_medals[i]'''
+            min_medals = num_medals[i]
 
-    print(num_medals[-1])
+    print(num_medals[-1])'''
